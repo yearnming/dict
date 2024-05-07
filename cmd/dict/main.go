aac0067055fa6084ac71adb2eb617bd65a928134
yearnming <48712331+yearnming@users.noreply.github.com>
2024-05-07 16:46:57 +0800

    测试

--- cmd/dict/main.go
+++ cmd/dict/main.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 
-	// TODO 接收参数 options := x.ParseOptions()
+	// 接收参数
 	options := runner.ParseOptions()
 
-	// TODO 创建一个生成器实例 NewRunner, err := runner.NewRunner(options)
+	// 创建一个生成器实例
 	NewRunner, err := runner.NewRunner(options)
 	if err != nil {
 		gologger.Fatal().Msgf("创建生成器实例错误: %s\n", err)
 	}
 
-	// TODO 运行生成器
+	// 运行生成器
 	err = NewRunner.Run()
 	if err != nil {
 		gologger.Fatal().Msgf("运行生成器错误: %s\n", err)
